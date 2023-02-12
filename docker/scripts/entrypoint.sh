492b0d1729105a57b09af700fa027d046149ba8c
katiyar-rahul <rahul.katiyar@deepx.co.jp>
2023-02-12 12:34:23 +0530

    feat: docker build action

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -ex
 
-RUN_XTERM=${RUN_XTERM:-yes}
-
-case $RUN_XTERM in
-  false|no|n|0)
-    rm -f /app/conf.d/xterm.conf
-    ;;
-esac
-
 # setup ros2 environment
-# source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/opt/ros/$ROS_DISTRO/setup.bash"
 
-exec /bin/tini -- /usr/bin/supervisord -n -c /app/supervisord.conf
+exec /usr/bin/tini -- /usr/bin/supervisord -n -c /app/supervisord.conf
