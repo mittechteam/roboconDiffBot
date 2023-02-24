a363071286a7af1910a4aeb984bf7e11f773024f
katiyar-rahul <rahulkatiyar19955@gmail.com>
2023-02-24 13:45:44 +0530

    feat: added source cmd to bashrc

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -4,4 +4,7 @@ set -ex
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
+# add source to bashrc
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+
 exec /usr/bin/tini -- /usr/bin/supervisord -n -c /app/supervisord.conf
