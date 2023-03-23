60312f714a00c437cbc71ccf7bf602bd30c76c42
Rahul Katiyar <rahulkatiyar19955@gmail.com>
2023-03-23 11:28:50 +0530

    ci: added lint check (#3)
    * ci: added lint check
    
    * feat: container settting changed
    
    * fix: lint errors
    
    * refactor: setup-ros ci
    
    * feat: added more linter

--- robot_description/launch/robot_rviz.launch.py
+++ robot_description/launch/robot_rviz.launch.py
@@ -1,15 +1,12 @@
 # launch file for rviz2
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
-# include description.launch.py
-from launch.actions import IncludeLaunchDescription
+
 def generate_launch_description():
-    
     # package
     robot_description_pkg_prefix = FindPackageShare('robot_description')
 
